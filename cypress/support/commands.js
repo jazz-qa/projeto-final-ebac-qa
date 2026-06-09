bdcbad2c83168607a89faf1b49a8872e18035f39
Jessica Bruna de Oliveira <jazzkoliver@gmail.com>
2026-06-09 16:39:31 -0300

    criando comando selecionarVariacao e corrigindo seletores nos comandos customizados

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,7 +5,7 @@ Cypress.Commands.add('login', (email, senha) => {
   cy.visit('/minha-conta/')
   cy.get('#username').type(email)
   cy.get('#password').type(senha)
-  cy.get('.woocommerce-form-login__submit').click()
+  cy.get('input[name="login"]').click()
 })
 
 // Limpa o carrinho antes de cada teste
@@ -21,10 +21,30 @@ Cypress.Commands.add('limparCarrinho', () => {
   })
 })
 
+// Seleciona a primeira variação disponível em produtos variáveis (ex: tamanho, cor)
+// Lê o data-product_variations do WooCommerce para garantir uma combinação válida e em estoque
+Cypress.Commands.add('selecionarVariacao', () => {
+  cy.get('body').then(($body) => {
+    if ($body.find('form.variations_form').length === 0) return
+
+    cy.get('form.variations_form').then(($form) => {
+      const variations = JSON.parse($form.attr('data-product_variations') || '[]')
+      const disponivel = variations.find((v) => v.is_in_stock && v.is_purchasable)
+
+      if (!disponivel) return
+
+      Object.entries(disponivel.attributes).forEach(([atributo, valor]) => {
+        cy.get(`select[name="${atributo}"]`).select(valor, { force: true })
+      })
+    })
+  })
+})
+
 // Adiciona produto ao carrinho pelo slug do produto
 Cypress.Commands.add('adicionarAoCarrinho', (slugProduto, quantidade = 1) => {
   cy.visit(`/produto/${slugProduto}/`)
-  cy.get('.quantity input').clear().type(String(quantidade))
+  cy.selecionarVariacao()
+  cy.get('input[type="number"]').clear().type(String(quantidade))
   cy.get('.single_add_to_cart_button').click()
   cy.get('.woocommerce-message, .wc-block-components-notice-banner').should('exist')
 })
